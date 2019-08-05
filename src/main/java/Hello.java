46e0ba112deceb917d2b3efdd7718bd74e128d79
tsantek <santek.vg@gmail.com>
2019-08-05 11:54:55 -0700

    init

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args){
+            // entry point to JAVA aplication
+        System.out.println("Hello world");
+
+        for(String argument : args){
+            System.out.println(argument);
+        }
+    }
+}
